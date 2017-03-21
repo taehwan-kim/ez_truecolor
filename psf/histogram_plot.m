566bb1d1a056c9158e9661621530099774828b2e
Taehwan Kim <taehwan@berkeley.edu>
2017-03-21 12:37:44 -0700

    crlb computation scripts added

--- psf/histogram_plot.m
+++ psf/histogram_plot.m
@@ -1,27 +1,27 @@
 clear all; 
 close all;
 
-temp = load('width_03_10.mat');
-width_03_10 = temp.width;
-temp = load('width_03_15.mat');
-width_03_15 = temp.width;
-temp = load('inten_03_10.mat');
-inten_03_10 = temp.inten;
-temp = load('inten_03_15.mat');
-inten_03_15 = temp.inten;
+temp = load('width_005_10.mat');
+width_005_10 = temp.width;
+temp = load('inten_005_10.mat');
+inten_005_10 = temp.inten;
+temp = load('width_005_20.mat');
+width_005_20 = temp.width;
+temp = load('inten_010_10.mat');
+inten_010_10 = temp.inten;
 
 figure(1);
 subplot(2,1,1);
-histfit(width_03_10);
-xlim([0.9 1.3]);
+histfit(width_005_10);
+% xlim([0.9 1.3]);
 subplot(2,1,2);
-histfit(width_03_15);
-xlim([0.9 1.3]);
+histfit(width_005_20);
+% xlim([0.9 1.3]);
 
 figure(2);
 subplot(2,1,1);
-histfit(inten_03_10);
-xlim([0.25 0.35]);
+histfit(inten_005_10);
+% xlim([0.25 0.35]);
 subplot(2,1,2);
-histfit(inten_03_15);
-xlim([0.25 0.35]);
+histfit(inten_010_10);
+% xlim([0.25 0.35]);
