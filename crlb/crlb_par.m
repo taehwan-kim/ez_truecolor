a2f6fd0f0a449620c3134e3f60f501c84e080926
Taehwan Kim <taehwan@berkeley.edu>
2017-03-21 14:51:48 -0700

    modified crlb parallel files

--- crlb/crlb_par.m
+++ crlb/crlb_par.m
@@ -1,10 +1,10 @@
 clc; clear all; close all;
 
-N = 100;
-NA = 1.4;
+N = 1500;
+NA = 1.25;
 nm = 1.5;
 z0 = 0e-9;
-lambda = 400e-9;
+lambda = 600e-9;
 pixelsize = 100e-9;
 numofpixels = 20;
 
@@ -22,16 +22,18 @@ dqdx = @(x, y) (2 / Az0) * ( uz0(sqrt(x.^2+y.^2), NA, lambda, nm, z0) .* dudx(x,
 dqdy = @(x, y) (2 / Az0) * ( uz0(sqrt(x.^2+y.^2), NA, lambda, nm, z0) .* dudx(y, x, NA, lambda, nm, z0) + vz0(sqrt(x.^2+y.^2), NA, lambda, nm, z0) .* dvdx(y, x, NA, lambda, nm, z0) ) ;
 dqdlambda = @(x, y) (2 / Az0) * ( uz0(sqrt(x.^2+y.^2), NA, lambda, nm, z0) .* dudlambda(sqrt(x.^2+y.^2), NA, lambda, nm, z0) + vz0(sqrt(x.^2+y.^2), NA, lambda, nm, z0) .* dvdlambda(sqrt(x.^2+y.^2), NA, lambda, nm, z0) ) - (1 / Az0) * dAz0dlambda * q(x, y);
 
-dmudxarray = zeros(numofpixels/2, numofpixels/2);
-dmudyarray = zeros(numofpixels/2, numofpixels/2);
-dmudlambdaarray = zeros(numofpixels/2, numofpixels/2);
-dmudNarray = zeros(numofpixels/2, numofpixels/2);
+dmudxarray = zeros(1, (numofpixels/2)^2);
+dmudyarray = zeros(1, (numofpixels/2)^2);
+dmudlambdaarray = zeros(1, (numofpixels/2)^2);
+dmudNarray = zeros(1, (numofpixels/2)^2);
 
-I33 = zeros(3,3);
+
 
 iter = ((numofpixels^2/4));
 
-for i=1:iter
+I33 = zeros(3,3,iter);
+
+parfor i=1:iter
 
     k = mod(i, numofpixels/2);
     if (k==0) 
@@ -39,28 +41,32 @@ for i=1:iter
     end
     j = ((i-k)/(numofpixels/2))+1;
 
-    dmudxarray(j,k) = 2*N*integral2(dqdx,pixelindex(j),pixelindex(j+1),pixelindex(k),pixelindex(k+1));
-    dmudyarray(j,k) = 2*N*integral2(dqdy,pixelindex(j),pixelindex(j+1),pixelindex(k),pixelindex(k+1));
-    dmudlambdaarray(j,k) = 2*N*integral2(dqdlambda,pixelindex(j),pixelindex(j+1),pixelindex(k),pixelindex(k+1));
-    dmudNarray(j,k) = integral2(q,pixelindex(j),pixelindex(j+1),pixelindex(k),pixelindex(k+1));
-
-    I33(1,1) = I33(1,1) + (4/(N*dmudNarray(j,k))) * dmudxarray(j,k) * dmudxarray(j,k);
-    I33(2,2) = I33(2,2) + (4/(N*dmudNarray(j,k))) * dmudyarray(j,k) * dmudyarray(j,k);
-    I33(3,3) = I33(3,3) + (4/(N*dmudNarray(j,k))) * dmudlambdaarray(j,k) * dmudlambdaarray(j,k);
-    I33(1,2) = I33(1,2) + (4/(N*dmudNarray(j,k))) * dmudxarray(j,k) * dmudyarray(j,k);
-    I33(1,3) = I33(1,3) + (4/(N*dmudNarray(j,k))) * dmudxarray(j,k) * dmudlambdaarray(j,k);
-    I33(2,3) = I33(2,3) + (4/(N*dmudNarray(j,k))) * dmudyarray(j,k) * dmudlambdaarray(j,k);
+    dmudxarray(i) = 2*N*integral2(dqdx,pixelindex(j),pixelindex(j+1),pixelindex(k),pixelindex(k+1));
+    dmudyarray(i) = 2*N*integral2(dqdy,pixelindex(j),pixelindex(j+1),pixelindex(k),pixelindex(k+1));
+    dmudlambdaarray(i) = 2*N*integral2(dqdlambda,pixelindex(j),pixelindex(j+1),pixelindex(k),pixelindex(k+1));
+    dmudNarray(i) = integral2(q,pixelindex(j),pixelindex(j+1),pixelindex(k),pixelindex(k+1));
 
 end
 
-I33(2,1) = I33(1,2);
-I33(3,1) = I33(1,3);
-I33(3,2) = I33(2,3);
-
-CRLB=I33^-1;
-
-sigmas = zeros(1,3);
-for i=1:3
-    sigmas(i) = sqrt(CRLB(i,i));
+for i=1:iter
+    
+    I33(1,1,i) = (4/(N*dmudNarray(i))) * dmudxarray(i) * dmudxarray(i);
+    I33(2,2,i) = (4/(N*dmudNarray(i))) * dmudyarray(i) * dmudyarray(i);
+    I33(3,3,i) = (4/(N*dmudNarray(i))) * dmudlambdaarray(i) * dmudlambdaarray(i);
+    I33(1,2,i) = (4/(N*dmudNarray(i))) * dmudxarray(i) * dmudyarray(i);
+    I33(1,3,i) = (4/(N*dmudNarray(i))) * dmudxarray(i) * dmudlambdaarray(i);
+    I33(2,3,i) = (4/(N*dmudNarray(i))) * dmudyarray(i) * dmudlambdaarray(i);
+    I33(2,1,i) = I33(1,2,i);
+    I33(3,1,i) = I33(1,3,i);
+    I33(3,2,i) = I33(2,3,i);
+    
 end
+
+
+%CRLB=I33^-1;
+
+%sigmas = zeros(1,3);
+%for i=1:3
+    %sigmas(i) = sqrt(CRLB(i,i));
+%end
 
